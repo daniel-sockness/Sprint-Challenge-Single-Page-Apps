0a01aa1c8cb04666c2f076042b6aca13b75d037e
daniel-sockness <danielsock@gmail.com>
2019-10-14 00:40:25 -0500

    Finished
    This was finished before time I forgot to comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
+import CharacterList from "./components/CharacterList.js";
+import LocationsList from './components/LocationsList';
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" render={props => <CharacterList {...props} />} />
+      <Route path="/locations" component={LocationsList} />
     </main>
   );
 }
